ba2335cf19d64f392c0606590a2bce2432b84129
cathy-gu <ygu.mfa2021@london.edu>
2020-10-05 19:07:57 +0100

    initial commit

---
+++ 20201004.R
@@ -0,0 +1,7 @@
+library(blogdown)
+blogdown::install_hugo()
+blogdown::new_site(theme = "MarcusVirg/forty",
+                   sample = TRUE,
+                   theme_example = TRUE,
+                   empty_dirs = TRUE,
+                   to_yaml = TRUE)
